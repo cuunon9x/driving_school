768e936a715beb3140235788481078bca7365e0b
anonymous <anonymous@anonymous.ai>
2026-03-26 20:35:47 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/driving_school/', // Set to your repo name
-  plugins: [react(), ghPages()]
+  plugins: [react()]
 });
